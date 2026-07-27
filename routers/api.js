5832df43028b546e2fb66bea584f1b9ec11a63fa
aureliacitra <citrapangukir@gmail.com>
2026-07-27 15:58:59 +0700

    Menambahkan routing autentikasi pengguna

--- routers/api.js
+++ routers/api.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const komikController = require('../controller/komikController');
 
+const komikController = require('../controller/komikController');
+const userController = require('../controller/userController');
+const authMidlleware = require('../controller/authMiddleware');
+
+// User routes
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+
+//public
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
+
+//protected
 router.post('/komik', komikController.createKomik);
 router.put('/komik/:id', komikController.updateKomik);
 router.delete('/komik/:id', komikController.deleteKomik);
