301a8cceed738de798de756bde2f9a14f5dd167b
aureliacitra <citrapangukir@gmail.com>
2026-07-27 15:00:18 +0700

    Menambhkan autentifikasi dan sinkronisasi db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,11 @@
 const db = require('../models');
 
 async function connectDatabase() {
-    
+  try {
+        await db.sequelize.authenticate();
+        console.log('Database connected successfully');
+
+        await db.sequelize.sync({ alter: true });
+        console.log('Database synchronized');
+
+     
