3ca40699b61563a22a6551dbef2f02aceaf4679d
aureliacitra <citrapangukir@gmail.com>
2026-07-27 16:12:29 +0700

    yg bener

--- routes/api.js
+++ routes/api.js
@@ -3,19 +3,18 @@ const router = express.Router();
 
 const komikController = require('../controller/komikController');
 const userController = require('../controller/userController');
-const authMidlleware = require('../controller/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // User routes
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 
-//public
+// Komik routes (protected)
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
 
-//protected
-router.post('/komik', komikController.createKomik);
-router.put('/komik/:id', komikController.updateKomik);
-router.delete('/komik/:id', komikController.deleteKomik);
+router.post('/komik', authMiddleware, komikController.createKomik);
+router.put('/komik/:id', authMiddleware, komikController.updateKomik);
+router.delete('/komik/:id', authMiddleware, komikController.deleteKomik);
 
 module.exports = router;
