2ec6090e7e3ebdb8f27941afdebbba45694b3cc6
aureliacitra <citrapangukir@gmail.com>
2026-07-27 14:54:19 +0700

    Menambahkan atribut pada model Komik

---
+++ models/komik.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        tittle: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+              author: {
+            type: DataTypes.STRING,
+        }
+    });
+    
